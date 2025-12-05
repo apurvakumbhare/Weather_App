9d3014ee1f22f59832ca7cfb5e4a33a371b48094
Apurva Kumbhare <kumbhareav@rknec.edu>
2025-12-05 09:23:20 +0530

    Controller Changed

--- src/main/java/com/example/WeatherApp/Controller.java
+++ src/main/java/com/example/WeatherApp/Controller.java
@@ -1,7 +1,27 @@
 package com.example.WeatherApp;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
+	public ChatClient chatClient;
+
+	public Controller(OllamaChatModel chatModel) {
+		this.chatClient=ChatClient.builder(chatModel).build();
+	}
+	@GetMapping("/chat")
+	
+	public ResponseEntity<String> chat(@RequestParam(name = "q") String query){
+		
+		return ResponseEntity.ok(
+				chatClient.prompt().user(query)
+				.advisors(new SimpleLoggerAdvisor())
+				.tools(new ToolsAndAll()).call().content());
+	}
 	
 }
